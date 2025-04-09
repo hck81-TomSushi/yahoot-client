0ebc98374a327d75e8899ad6f6405393183ca490
dikinovtrianda <dikinovtrianda@gmail.com>
2025-04-09 08:18:23 +0700

    feat : username authentication

---
+++ src/layouts/AuthenLayout.jsx
@@ -0,0 +1,39 @@
+import { Outlet, useNavigate } from "react-router";
+import { yahootServer } from "../../helpers/http-client";
+import { useEffect, useState } from "react";
+
+export default function AuthenLayout() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkAuthentication = async () => {
+            const access_token = localStorage.getItem("access_token");
+            if (!access_token) {
+                navigate("/login");
+                return;
+            }
+            try {
+                const { data } = await yahootServer.get("/username", {
+                    headers: { Authorization: access_token }
+                });
+                if (data.username) {
+                    setIsAuthenticated(true);
+                } else {
+                    localStorage.removeItem("access_token");
+                    navigate("/login");
+                }
+            } catch (error) {
+                localStorage.removeItem("access_token");
+                navigate("/login");
+            }
+        };
+        checkAuthentication();
+    }, [navigate]);
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+    return <Outlet />;
+}

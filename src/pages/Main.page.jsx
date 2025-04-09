8612e85977bd2526a4dc898894396c109e5d3a85
Marha Nur Amalina <marhanura@gmail.com>
2025-04-09 10:54:59 +0700

    Feat: Landing page pisah login page

--- src/pages/Main.page.jsx
+++ src/pages/Main.page.jsx
@@ -1,19 +1,19 @@
 import { useNavigate } from "react-router";
 import { yahootServer } from "../../helpers/http-client";
 import YahootLogo from "../assets/yahoot_white.png";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Swal from "sweetalert2";
 
 export default function MainPage() {
-  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
   const inputUsername = async () => {
     try {
-      const { data } = await yahootServer.post("/login", { username: name });
+      const { data } = await yahootServer.post("/login", { username });
       localStorage.setItem("access_token", "Bearer " + data.access_token);
       setUsername(data.username);
+      navigate("/");
     } catch (error) {
       console.log("🐄 - inputUsername - error:", error);
       Swal.fire({
@@ -25,73 +25,46 @@ export default function MainPage() {
     }
   };
 
-  const checkUsername = async () => {
-    const access_token = localStorage.getItem("access_token");
-    if (access_token) {
-      try {
-        const { data } = await yahootServer.get("/username", {
-          headers: { Authorization: localStorage.getItem("access_token") },
-        });
-        setUsername(data.username);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  const deleteUsername = () => {
-    localStorage.removeItem("access_token");
-    setUsername("");
-  };
-
-  useEffect(() => {
-    checkUsername();
-  }, []);
-
   return (
     <section className="chalkboard">
+      <a href="/" className="btn btn-success border-b-4 absolute top-4 left-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+      </a>
+
       <div className="flex flex-col items-center justify-evenly h-full">
         <img src={YahootLogo} alt="Yahoot Logo" className="h-15" />
-        {username ? (
-          <h1 className="text-3xl">Hello, {username}!</h1>
-        ) : (
-          <form onSubmit={() => inputUsername()}>
-            <label className="input validator my-4">
-              <input
-                type="text"
-                className="text-center text-accent text-xl"
-                required
-                placeholder="Enter your username"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </label>
-            <button
-              className="btn btn-accent btn-lg text-3lg w-80 h-15"
-              type="button"
-              onClick={() => inputUsername()}
-            >
-              I want this name!
-            </button>
-          </form>
-        )}
-        {username && (
-          <>
-            <button
-              className="btn btn-accent btn-lg text-2lg w-80 h-15"
-              type="button"
-              onClick={() => deleteUsername()}
-            >
-              I want to change my name!
-            </button>
-            <button
-              className="btn btn-accent btn-xl text-3xl w-100 h-30"
-              onClick={() => navigate("/waiting-room")}
-            >
-              Start Playing!
-            </button>
-          </>
-        )}
+        <form onSubmit={() => inputUsername()}>
+          <label className="input validator my-4">
+            <input
+              type="text"
+              className="text-center text-accent text-xl"
+              required
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <button
+            className="btn btn-accent btn-lg text-3lg w-80 h-15"
+            type="button"
+            onClick={() => inputUsername()}
+          >
+            I want this name!
+          </button>
+        </form>
       </div>
     </section>
   );

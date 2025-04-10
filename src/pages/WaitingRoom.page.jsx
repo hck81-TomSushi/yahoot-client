3be3beb75af6315694f56ac4c7416598fbf55726
Marha Nur Amalina <marhanura@gmail.com>
2025-04-10 09:37:31 +0700

    Refactor: Home Button, Fix: Countdown SFX

--- src/pages/WaitingRoom.page.jsx
+++ src/pages/WaitingRoom.page.jsx
@@ -3,8 +3,8 @@ import { useUsername } from "../contexts/username.context";
 import { socket } from "../../helpers/socket";
 import { useNavigate } from "react-router";
 import YahootLogo from "../assets/yahoot_white.png";
-import BGM from "../assets/main_bgm.mp3";
-import CountdownSound from "../assets/countdown.mp3";
+import CountdownSound from "../assets/countdown_tick.mp3";
+import HomeButton from "../components/HomeButton";
 
 export default function WaitingRoom() {
   const { username, userCode, setUsername } = useUsername();
@@ -12,7 +12,6 @@ export default function WaitingRoom() {
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
   const audioRef = useRef(null);
-  const countdownSound = new Audio(CountdownSound);
 
   useEffect(() => {
     socket.on("user queue", (roomData) => {
@@ -21,7 +20,7 @@ export default function WaitingRoom() {
     });
 
     socket.emit("game queue", { username, userCode });
-    
+
     socket.on("countdown", (time) => {
       setCountdown(time);
     });
@@ -35,7 +34,7 @@ export default function WaitingRoom() {
       socket.off("game queue");
     };
   }, []);
-  
+
   useEffect(() => {
     function playAudio() {
       const audio = audioRef.current;
@@ -43,34 +42,15 @@ export default function WaitingRoom() {
         audio.play().catch((error) => {
           console.error("Error playing audio:", error);
         });
-        if (countdown > 0) {
-          audio.pause();
-        }
       }
     }
     playAudio();
-  }, [countdown]);
-  
+  }, []);
+
   return (
     <section className="chalkboard">
-      <audio ref={audioRef} src={BGM} loop autoPlay />
-      <a onClick={() => navigate('/')} className="btn btn-success border-b-4 absolute top-4 left-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-          />
-        </svg>
-      </a>
-
+      <audio ref={audioRef} src={CountdownSound} loop autoPlay />
+      <HomeButton />
       <div className="h-full flex flex-col items-center justify-evenly">
         <img src={YahootLogo} alt="Yahoot Logo" className="h-15" />
         <h1 className="text-2xl">
@@ -78,7 +58,7 @@ export default function WaitingRoom() {
           <br />
           Pengetahuan Umum
         </h1>
-        
+
         <p>Countdown : {countdown} second</p>
         <div className="flex flex-row gap-4 items-center justify-items-center">
           {users.map((user, index) => (

0155df49cceb2e7c8dde17cd0938e94b1c79fa8f
Hilmi Ahmad <hilmiadd@gmail.com>
2025-04-10 12:02:50 +0700

    fix: api and socket url

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,6 +1,15 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+// export const socket = io("http://localhost:3000", {
+//   auth: {
+//     token: localStorage.getItem("access_token"),
+//   },
+//   reconnection: true,
+//   reconnectionAttempts: 5,
+//   reconnectionDelay: 1000,
+// });
+
+export const socket = io("https://server-yahoot.hilminever.online/", {
   auth: {
     token: localStorage.getItem("access_token"),
   },
